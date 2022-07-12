6082a4c3ac1851a9b302625593a33b39a3fe65fc
danisteb90 <esteban.alban9003@gmail.com>
2022-07-12 18:24:02 -0500

    first commit

---
+++ NumerosJS.js
@@ -0,0 +1,13 @@
+let alturaCm = 168;
+let alturaM = 1.68;
+let peso = 70.7;
+let alturaRed = Math.ceil(alturaM);
+let pesoRed = Math.floor(peso);
+let valorMax = `El valor máximo que se puede obtener en JavaScript + 1 = ${(Number.MAX_VALUE)}`;
+
+console.log(alturaCm);
+console.log(alturaM);
+console.log(peso);
+console.log(alturaRed);
+console.log(pesoRed);
+console.log(valorMax);
